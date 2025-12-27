22c74222e011ded6a99e62b2af2bc2f7560072ca
Msuchecki <m.suchecki2002@gmail.com>
2025-12-27 15:47:01 +0100

    buildings retexture

--- entities/buildings/keep/keep.gd
+++ entities/buildings/keep/keep.gd
@@ -1,5 +1,5 @@
 extends Building
 
 func _set_actions() -> void:
-    actions[0] = load("res://ui/panels/action/buttons/build/build_tent_button.tscn")
-    actions[1] = load("res://ui/panels/action/buttons/build/build_farm_button.tscn")
+	actions[0] = load("res://ui/panels/action/buttons/build/build_tent_button.tscn")
+	actions[1] = load("res://ui/panels/action/buttons/build/build_farm_button.tscn")
--- world/game.gd
+++ world/game.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
 func _ready() -> void:
-    ControlGroupSystem.enabled = true
+	ControlGroupSystem.enabled = true
 
 func _exit_tree() -> void:
-    ControlGroupSystem.clear_groups()
-    ControlGroupSystem.enabled = false
-    SelectionManager.clear_selection_silently()
+	ControlGroupSystem.clear_groups()
+	ControlGroupSystem.enabled = false
+	SelectionManager.clear_selection_silently()
